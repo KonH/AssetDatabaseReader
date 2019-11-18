5bc68e0addb302be63f79cfdb47e2bd8971b82e8
KonH <konh@yandex.ru>
2019-11-18 22:10:46 +0700

    Placeholder for further work

---
+++ Assets/Scripts/Editor/AssetDatabaseReader.cs
@@ -0,0 +1,5 @@
+public class AssetDatabaseReader {
+	public string[] CollectDependencies(string assetPath) {
+		return new [] { "some invalid result" };
+	}
+}
--- Assets/Scripts/Editor/Tests.cs
+++ Assets/Scripts/Editor/Tests.cs
@@ -1,6 +1,30 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
 
 public class Tests {
-	[Test]
-	public void TestsSimplePasses() {}
+	static string[] DefaultSolution(string path) {
+		var asset = AssetDatabase.LoadAssetAtPath<Object>(path);
+		var roots = new [] { asset };
+		var dependencies = EditorUtility.CollectDependencies(roots);
+		return dependencies.Select(obj => AssetDatabase.GetAssetPath(obj)).ToArray();
+	}
+
+	static string[] CustomSolution(string path) {
+		return new AssetDatabaseReader().CollectDependencies(path);
+	}
+
+	static string[] GetAssetPaths() {
+		return AssetDatabase.GetAllAssetPaths();
+	}
+
+	[TestCaseSource("GetAssetPaths")]
+	public static void IsCustomSolutionMakeSameResult(string path) {
+		var defaultResult = DefaultSolution(path);
+		var customResult = CustomSolution(path);
+		Assert.NotNull(defaultResult);
+		Assert.NotNull(customResult);
+		CollectionAssert.AreEquivalent(defaultResult, customResult);
+	}
 }
